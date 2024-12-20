2ea0f52b1f047b6aa6059b0de182af1cca9ac1dc
Claudia Zarzuela <100291375+ClaudiaZarzuela@users.noreply.github.com>
2024-12-20 16:58:31 +0100

    Cambio de escritura Record

--- AAKart_Enunciado/Assets/Karting/Scripts/ML-UCM/Record.cs
+++ AAKart_Enunciado/Assets/Karting/Scripts/ML-UCM/Record.cs
@@ -256,7 +256,6 @@ public class Record : MonoBehaviour
         {
             Parameters p = parameters[i];
             csv += p.ToString();
-            csv += ",";
             csv += labels[i].ToString() + "\n";
         }
 
